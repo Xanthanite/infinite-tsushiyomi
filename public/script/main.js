48408a7200451e2efe5a2228948fb81ce8dde66c
Xanthanite <41843815+Xanthanite@users.noreply.github.com>
2020-09-23 12:55:11 -0400

    Made a small touch to the navbar home links with smooth scroll as I determined it's probably not necessary to include the first section.

--- public/script/main.js
+++ public/script/main.js
@@ -76,86 +76,73 @@ $(document).ready(function() {
   setInputFilter($(".phoneInput"), function(value) {
     return /^\d*\-*\d*\-*\d*$/.test(value); // Allow digits and '-' only in a specific format, using a RegExp
   });
-})
                                                               //Smooth Scroll Handling
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .not('[href="/"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .not('[href="/"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
       }
-    }
-  });
+    });
 
-                                                                            //Homepage Image Overlay Idler
+                                                                              //Homepage Image Overlay Idler
 
 
-$(".grid-img-overlay").each(function() {//selectiong all the grid image overlays
-  $(this).on("mouseout", function() {//on mouse out so the overlay goes away when you hover out of it
-    if ($(this).css("opacity") > "0") {//opacity can be greater than 0 since there could be a fade out being triggered while a fade in is triggered
-      clearTimeout(idleListener);
-      $(this).css("opacity", "0")
-    }
-  });
-  $(this).on("mousemove", function() {//activates the overlay whenever the mouse is moved over the image
-      if ($(this).css("opacity") <= "1"){//less than or equal to one due to possible overlapping transitions
-        $(".grid-img-overlay").css("opacity", "0");
-        lastIdle = currentIdle;
+  $(".grid-img-overlay").each(function() {//selectiong all the grid image overlays
+    $(this).on("mouseout", function() {//on mouse out so the overlay goes away when you hover out of it
+      if ($(this).css("opacity") > "0") {//opacity can be greater than 0 since there could be a fade out being triggered while a fade in is triggered
         clearTimeout(idleListener);
-        $(this).css("opacity", "1");
-        idleListener = setTimeout(function() {  
-          if($(this).css("opacity") <= "1"){
-              currentIdle = idleListener;
-              console.log(currentIdle);
-              $(this).css("opacity", "0");
-          }
-        }.bind(this), 1500);//Binded this here as I was receiving errors due to scope
-      } else if ($(this).css("opacity") == "1") {
-      clearTimeout(idleListener);
-    }
-  })
+        $(this).css("opacity", "0")
+      }
+    });
+    $(this).on("mousemove", function() {//activates the overlay whenever the mouse is moved over the image
+        if ($(this).css("opacity") <= "1"){//less than or equal to one due to possible overlapping transitions
+          $(".grid-img-overlay").css("opacity", "0");
+          lastIdle = currentIdle;
+          clearTimeout(idleListener);
+          $(this).css("opacity", "1");
+          idleListener = setTimeout(function() {  
+            if($(this).css("opacity") <= "1"){
+                currentIdle = idleListener;
+                console.log(currentIdle);
+                $(this).css("opacity", "0");
+            }
+          }.bind(this), 1500);//Binded this here as I was receiving errors due to scope
+        } else if ($(this).css("opacity") == "1") {
+        clearTimeout(idleListener);
+      }
+    })
+  });
 });
-                                                                  //Reservation Page Form handling (moved to onpageload)
-// $('#contact-submit').on('click', function() {
-//   setTimeout(function() {
-//     $('#hp-contact-form-inner').trigger('reset')
-//   }, 10)
-// })
-
-// $('#reserve-submit').on('click', function() {
-//   setTimeout(function() {
-//     $('#rp-contact-form-inner').trigger('reset')
-//   }, 10)
-// })
-
 
 
